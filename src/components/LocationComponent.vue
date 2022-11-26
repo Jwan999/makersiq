d8e3f03d96d4562a6374df0c9cbfd7c1e90d59cc
Jwan999 <jwanaalfatla1999@gmail.com>
2022-11-26 16:01:36 +0300

    main

--- src/components/LocationComponent.vue
+++ src/components/LocationComponent.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <div>where you can find us</div>
+  <div class="my-44">
+    <h1 class="border border-orange text-orange py-3 text-center uppercase font-bold text-xl">ً
+      Our location
+    </h1>
+
+    <div class="px-24 mt-24">
+      test
+    </div>
+
   </div>
 </template>
 
